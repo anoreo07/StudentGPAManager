597768ad268f926455a2385a33898b4c4c08392c
AnOreo0207 <pejij2009@gmail.com>
2026-02-07 05:40:49 +0700

    Initial Commit

---
+++ StudentGPAManager/StudentGPAManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentGPAManagerApp.swift
+//  StudentGPAManager
+//
+//  Created by Nguyễn Hải An on 7/2/26.
+//
+
+import SwiftUI
+
+@main
+struct StudentGPAManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StudentGPAManagerTests/StudentGPAManagerTests.swift
@@ -0,0 +1,16 @@
+//
+//  StudentGPAManagerTests.swift
+//  StudentGPAManagerTests
+//
+//  Created by Nguyễn Hải An on 7/2/26.
+//
+
+import Testing
+
+struct StudentGPAManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
